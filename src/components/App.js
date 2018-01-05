147553e3a7ffc909b33e1478934fa4255f3614a9
hazalozturk <hcanisag@gmail.com>
2018-01-04 21:38:28 -0500

    Create first Bootstrap Layout

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,17 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-      null
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h1> Name </h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4"> A </div>
+          <div className="col-md-8"> B </div>
+        </div>
+      </div>
     );
   }
 }
